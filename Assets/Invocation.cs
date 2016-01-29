66a78297ac30e7282e0f4786847065e0b18b92bb
unknown <pi.martinez@J001-0137-11.etudiant.rubika-edu.com.local>
2016-01-29 21:35:21 +0100

    first invocation script

---
+++ Assets/Invocation.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Invocation : MonoBehaviour {
+
+    public GameObject invocPrefab;
+
+    public float invocationRadius = 5f;
+
+    public float detectionRadius = 5f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            Invoc();
+        }
+	}
+
+    void Invoc()
+    {
+        Vector3 spawnPosition;
+        float x = Random.Range(transform.position.x - invocationRadius, transform.position.x + invocationRadius);
+        float z = Random.Range(transform.position.z - invocationRadius, transform.position.z + invocationRadius);
+
+        spawnPosition = new Vector3(x, invocPrefab.transform.localScale.y / 2, z);
+
+        GameObject go = Instantiate(invocPrefab, spawnPosition, Quaternion.identity) as GameObject;
+        
+        Vector3 center = go.transform.position;
+        
+        Collider[] hitColliders = Physics.OverlapSphere(center, detectionRadius);
+
+        bool canExist = true;
+
+        foreach(Collider col in hitColliders)
+        {
+            Debug.Log(col.tag);
+
+            if (col.tag != "Ground" && col.gameObject != go)
+            {
+                canExist = false;
+            }
+        }
+
+        if (!canExist)
+        {
+            Destroy(go);
+        }
+    }
+}
