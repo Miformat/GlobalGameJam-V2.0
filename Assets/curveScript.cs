8c16b52d9d016ff5845333e7b50c409fd2f2d6ce
Miformat <m.humeau@J001-0136-12.etudiant.rubika-edu.com.local>
2016-01-29 23:05:52 +0100

    curve

---
+++ Assets/curveScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class curveScript : MonoBehaviour {
+
+
+	public AnimationCurve sizecurve ;
+	float Timer;
+
+
+	// Use this for initialization
+	void Start () {
+		Timer = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Timer += Time.deltaTime;
+		transform.localScale = Vector3.one * sizecurve.Evaluate(Timer);
+		//if (Timer > 1.0f){Timer = 0;;}
+	}
+}
